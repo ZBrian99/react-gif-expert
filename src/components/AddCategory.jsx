bb62408adc87db8a1298d592ec133083ab896357
ZBrian199 <brianzelada1999@gmail.com>
2022-11-29 09:43:27 -0300

    Primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export const AddCategory = ({ onNewCategory }) => {
+	const [inputValue, setInputValue] = useState('');
+
+	const onInputChange = ({ target }) => {
+		setInputValue(target.value);
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		if (inputValue.trim().length < 1) return;
+		// setCategories((categories) => [inputValue, ...categories]);
+    onNewCategory(inputValue.trim());
+		setInputValue('');
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<input autoFocus onChange={onInputChange} type='text' placeholder='Buscar gifs' value={inputValue} />
+		</form>
+	);
+};
